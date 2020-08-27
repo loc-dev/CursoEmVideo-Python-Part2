9a93414bb8f0c587fc32b879dd2bac56a44ec74a
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-08-27 15:43:11 -0400

    Relembrando a Condição Composta

---
+++ Fase12/01_Condicoes_02.py
@@ -0,0 +1,11 @@
+# Fase 12 - Condições Aninhadas
+# Prática
+
+nome = str(input('Qual é o seu nome? '))
+
+# Condição Composta
+if nome == 'Gustavo':
+    print('Que nome bonito!')
+else:
+    print('Seu nome é bem normal.')
+print('Tenha um bom dia, {}!'.format(nome))
