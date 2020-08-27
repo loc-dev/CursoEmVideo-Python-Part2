808c65e21b64a663e0f4b27eb5b472b029b19cdc
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-08-27 15:36:05 -0400

    Relembrando a Condição Simples

---
+++ Fase12/01_Condicoes.py
@@ -0,0 +1,9 @@
+# Fase 12 - Condições Aninhadas
+# Prática
+
+nome = str(input('Qual é o seu nome? '))
+
+# Condição Simples
+if nome == 'Leonardo':
+    print('Que nome bonito!')
+print('Tenha um bom dia, {}!'.format(nome))
