4f9e5a804cccd955c0c0604772df950c8da5fd2f
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-09-07 15:53:57 -0400

    Estrutura de repetição for, com a função input dentro do laço, realizando somatória dos números atribuidos

---
+++ Fase13/03_RepeticaoInput_02.py
@@ -0,0 +1,8 @@
+# Fase 13 - Estrutura de repetição For - Parte 1
+# Prática
+
+s = 0
+for c in range(0, 4):
+    n = int(input('Digite um valor: '))
+    s += n
+print('O somatório de todos os valores foi {}'.format(s))
