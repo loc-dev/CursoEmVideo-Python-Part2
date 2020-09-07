ace4b3e7badb1ea9eaef088a1a1a6911bbba041b
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-09-07 15:26:44 -0400

    Aprendendo a estrutura de repetição for, conhecendo a sintaxe da função range()

---
+++ Fase13/01_Repeticao.py
@@ -0,0 +1,28 @@
+# Fase 13 - Estrutura de repetição For - Parte 1
+# Prática
+
+# Imprimindo 'Oi' 6 vezes
+for c in range(0, 6):
+    print('Oi')
+print('FIM')
+
+print('')
+
+# Imprimindo uma contagem de 1 até 6
+for c in range(1, 7):
+    print(c)
+print('FIM')
+
+print('')
+
+# Imprimindo uma contagem regressiva
+for c in range(6, 0, -1):
+    print(c)
+print('FIM')
+
+print('')
+
+# Imprimindo uma contagem de 0 até 6 incrementando em 2
+for c in range(0, 7, 2):
+    print(c)
+print('FIM')
