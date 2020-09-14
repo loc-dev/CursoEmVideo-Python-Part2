804d73b6e9b632e3db8f130cbd49971c202f8e1f
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-09-14 16:02:03 -0400

    Utilizando o exemplo com a estrutura de repetição while

---
+++ Fase14/02_RepeticaoWhile.py
@@ -0,0 +1,11 @@
+# Fase 14 - Estrutura de repetição While - Parte 2
+# Prática
+
+# Utilizando o exemplo anterior da estrutura de repetição For
+
+c = 1
+
+while c < 10:
+    print(c)
+    c += 1
+print('Fim')
