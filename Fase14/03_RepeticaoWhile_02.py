e4150f2a319075305eac71fed57fe0c383b62f6a
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-09-14 16:18:17 -0400

    Conhecendo o flag ou condição de parada na estrutura de repetição while

---
+++ Fase14/03_RepeticaoWhile_02.py
@@ -0,0 +1,10 @@
+# Fase 14 - Estrutura de repetição While - Parte 2
+# Prática
+
+# Limite desconhecido, mas, ao digitar o valor zero o programa finaliza
+
+n = 1
+
+while n != 0:
+    n = int(input('Digite um valor: '))
+print('Fim')
