a36f409b062474ba720ec03152b68f6cf2468b7d
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-09-28 14:59:56 -0400

    Compreendendo o funcionamento do Loop Infinito - While True

---
+++ Fase15/01_Loop.py
@@ -0,0 +1,12 @@
+# Fase 15 - Interrompendo a estrutura de repetições While
+# Prática
+
+# Funcionamento do Loop Infinito
+
+cont = 1
+
+while True:
+    print(cont, ' -> ', end='')
+    cont += 1
+
+print('Acabou')
