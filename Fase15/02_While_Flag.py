9d200af26123e55b8367daa129d3923241799d1b
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-09-28 15:15:45 -0400

    Relembrando a estrutura while com flag

---
+++ Fase15/02_While_Flag.py
@@ -0,0 +1,10 @@
+# Fase 15 - Interrompendo a estrutura de repetições While
+# Prática
+
+# Relembrando a Estrutura While com Flag
+
+n = 0
+
+while n != 999:
+    n = int(input('Digite um número: '))
+
