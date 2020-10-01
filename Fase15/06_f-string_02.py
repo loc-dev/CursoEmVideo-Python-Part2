5cc0908a81bb547b0e09092d8147b7e9f417f6dc
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-10-01 17:42:44 -0400

    Exemplo de utilização com f-strings

---
+++ Fase15/06_f-string_02.py
@@ -0,0 +1,10 @@
+# Fase 15 - Interrompendo a estrutura de repetições While
+# Prática
+
+# Mais alguns exemplos com f-strings
+
+nome = "Leonardo"
+idade = 23
+salario = 1250.00
+
+print(f'O {nome} tem {idade} anos, ele está ganhando R${salario:.2f}')
